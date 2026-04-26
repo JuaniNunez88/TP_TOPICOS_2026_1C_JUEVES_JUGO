263d525cb33ffbbc42f1e33191f4430f3bcd1b66
JuaniNunez88 <jnunez280@alumno.unlam.edu.ar>
2026-04-26 18:03:48 -0300

    Creacion de proyecto

---
+++ Tetris/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+    Apellido: Nuñez, Juan Ignacio
+    DNI: 44547280
+    Usuario: JuaniNunez88
+    Entrega: Sí
+
+    Apellido: Delia, Lautaro Tomas
+    DNI: 43722468
+    Usuario: Iautaro
+    Entrega: Sí
+
+    Apellido: Borja, Tomas Hernan
+    DNI: 42353302
+    Usuario: TomiiHB
+    Entrega: Sí
+*/
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
