f4be3cae42b3d44a2d98b0c0aa03d3530642f710
Iautaro <ladelia@alumno.unlam.edu.ar>
2026-05-10 15:19:05 -0300

    Implementacion de presionar/sostener tecla. Ahora conviven ambas funciones de manera correcta para las teclas:izq, der y abajo.

--- Tetris/deteccion_tecla.c
+++ Tetris/deteccion_tecla.c
@@ -1,35 +1,4 @@
-/*
-#include "deteccion_tecla.h"
-#include "GBT/gbt.h"
 
-static int izq, der, abajo, enter, pausa, arriba, salir;
-
-void input_actualizar()
-{
-    enter  = gbt_tecla_presionada(GBTK_ENTER);
-    pausa  = gbt_tecla_presionada(GBTK_p);
-    //izq    = gbt_tecla_sostenida(GBTK_IZQUIERDA);
-    //der    = gbt_tecla_sostenida(GBTK_DERECHA);
-    abajo  = gbt_tecla_sostenida(GBTK_ABAJO);
-
-    izq    = gbt_tecla_presionada(GBTK_IZQUIERDA);
-    der    = gbt_tecla_presionada(GBTK_DERECHA);
-
-    arriba = gbt_tecla_presionada(GBTK_ARRIBA);
-    salir = gbt_tecla_presionada(GBTK_ESCAPE);
-}
-
-int input_izquierda() { return izq;    }
-int input_derecha()   { return der;    }
-int input_abajo()     { return abajo;  }
-int input_arriba()    { return arriba; }
-int input_enter()     { return enter;  }
-int input_pausa()     { return pausa;  }
-
-int input_salir()     { return salir;  }
-
-
-*/
 #include "deteccion_tecla.h"
 #include "GBT/gbt.h"
 
